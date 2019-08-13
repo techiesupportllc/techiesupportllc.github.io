2dfe63af1511101684aa46215a8dd8955fa23652
Ben Gilstrap <th3str4p@gmail.com>
2019-08-13 16:34:21 -0700

    added more copy to the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,24 +6,28 @@ export default () => (
     <div style={{ textAlign: "center" }}>
       <h1>Techie Support</h1>
       <p style={{ fontWeight: "bold" }}>Radically different web development</p>
-      <h2>What do we do?</h2>
-      <p>We provide <b>premium US-based</b> web development at a <b>fraction of the cost</b> of hiring a full-time web developer.  Our sites are <b>fast, secure and clean</b>.</p>
-      <h2>Why choose us?</h2>
+      <h2>What will we do for you?</h2>
+      <p>We'll provide you with <b>premium US-based</b> web development at a <b>fraction of the cost</b> of hiring a full-time web developer.  Your site will be <b>fast, secure and clean</b>.</p>
+      <h2>Why will you choose us?</h2>
       <ul>
         <li>You want <b>premium quality</b></li>
         <li>You want a <b>US-based company</b></li>
         <li>You want a <b>more affordable</b> option than hiring a full-time web developer</li>
       </ul>
-      <h2>How much do we cost?</h2>
+      <h2>How much will we cost you?</h2>
       <p>You'll receive all of the above benefits for a <b>flat monthly rate of $599</b>.  No upcharges, no nickel and diming, no headaches.</p>
-      <h2>Why do we cost so much?</h2>
-      <h3>Here's the short answer:</h3>
+      <h3>Why do we cost so much?</h3>
+      <h4>Here's the short answer:</h4>
       <p><b>We don't.</b></p>
-      <h3>Here's the long answer:</h3>
+      <h4>Here's the long answer:</h4>
       <p>You could <b>roll the dice on hiring some cheap overseas labor</b> to build your site -- maybe from a marketplace like Upwork or Fiverr.  But if you aren't sure what to look for and you don't have the time to properly vet those freelancers, <b>you're going to have a poor experience more often than not</b>.</p>
       <p>You could <b>take a chance on hiring a full-time web developer</b> to build your site -- maybe with an ad on Craigslist.  But then you have to <b>provide equipment and benefits</b>, not to mention needing to <b>manage a new employee</b>.</p>
       <p>You could <b>save yourself time, money and hassle by hiring us</b> to build your site.  There's <b>no vetting for you to waste your time doing</b>.  There's <b>no expensive equipment or benefits for you to waste money providing</b>.  There's <b>no PTO or other headaches for you to waste brainpower managing</b>.</p>
       <p>To top it off, <b>you'll only spend what it would cost you to hire a part-time employee at the federal minimum wage</b>.  And let's be realistic -- <b>you're not going to get premium-quality talent by hiring minimum-wage employees</b>.</p>
+      <h2>How long will it take us to build your site?</h2>
+      <p>Most sites take <b>between one and three months</b> to build from start to finish.</p>
+      <h2>Why will you continue to pay us monthly after your site is built?</h2>
+      <p>Our goal is to <b>build long-term relationships with our clients</b>.  Therefore, <b>the benefits you receive</b> by hiring us <b>don't stop when your site is done</b>.  Your monthly cost also includes <b>blazing fast hosting</b> along with an <b>SSL certificate</b> to keep your site secure.  In addition, you'll continue to receive <b>ongoing maintenance and support</b> for any tweaks you'd like to make to your site.</p>
     </div>
   </Layout>
 )
