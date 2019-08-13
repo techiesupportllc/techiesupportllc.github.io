498b3a83f9d8631c22c92724d673c179af969afd
Ben Gilstrap <th3str4p@gmail.com>
2019-08-13 16:02:58 -0700

    started laying out the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,27 @@ import Layout from "../components/layout"
 
 export default () => (
   <Layout>
-    <h1>Hi! I'm building a site.</h1>
-    <p>What do I like to do?  Build websites!</p>
+    <div style={{ textAlign: "center" }}>
+      <h1>Techie Support</h1>
+      <p style={{ fontWeight: "bold" }}>Radically different web development</p>
+      <h2>What do we do?</h2>
+      <p>We provide <b>premium US-based</b> web development at a <b>fraction of the cost</b> of hiring a full-time web developer.  Our sites are <b>fast, secure and clean</b>.</p>
+      <h2>Why choose us?</h2>
+      <ul>
+        <li>You want <b>premium quality</b></li>
+        <li>You want a <b>US-based company</b></li>
+        <li>You want a <b>more affordable</b> option than hiring a full-time web developer</li>
+      </ul>
+      <h2>How much do we cost?</h2>
+      <p>You'll receive all of the above benefits for a <b>flat monthly rate of $599</b>.  No upcharges, no nickel and diming, no headaches.</p>
+      <h2>Why do we cost so much?</h2>
+      <h3>Here's the short answer:</h3>
+      <p><b>We don't.</b></p>
+      <h3>Here's the long answer:</h3>
+      <p>You could <b>roll the dice on hiring some cheap overseas labor</b> to build your site -- maybe from a marketplace like Upwork or Fiverr.  But if you aren't sure what to look for and you don't have the time to properly vet those freelancers, <b>you're going to have a poor experience more often than not</b>.</p>
+      <p>You could <b>take a chance on hiring a full-time web developer</b> to build your site -- maybe with an ad on Craigslist.  But then you have to <b>provide equipment and benefits</b>, not to mention needing to <b>manage a new employee</b>.</p>
+      <p>You could <b>save yourself time, money and hassle by hiring us</b> to build your site.  There's <b>no vetting for you to waste your time doing</b>.  There's <b>no expensive equipment or benefits for you to waste money providing</b>.  There's <b>no PTO or other headaches for you to waste brainpower managing</b>.</p>
+      <p>To top it off, <b>you'll only spend what it would cost you to hire a part-time employee at the federal minimum wage</b>.  And let's be realistic -- <b>you're not going to get premium-quality talent by hiring minimum-wage employees</b>.</p>
+    </div>
   </Layout>
 )
