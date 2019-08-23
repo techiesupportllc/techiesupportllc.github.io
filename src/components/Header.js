cbbee26073d2ae05ca446c2130069e00cf9a9f80
Ben Gilstrap <th3str4p@gmail.com>
2019-08-23 14:58:15 -0700

    added login link to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,13 +21,41 @@ const HeaderDiv = styled.div`
   }
 `;
 
+const LoginDiv = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  padding: 0 1rem 0 1rem;
+  @media screen and (max-width: 600px) {
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem 1rem 0 1rem;
+  }
+`;
+
+const LoginLink = styled.a`
+  font-size: 0.8rem;
+  color: #3D4852;
+  border-bottom: 1px solid #3D4852;
+  :hover {
+    text-decoration: none;
+  }
+`;
+
 const Header = props => (
+  <>
   <HeaderDiv>
     <Link to="/">
       <img src={Logo} height="40" />
     </Link>
     <NavButtons />
   </HeaderDiv>
+  <LoginDiv>
+    <LoginLink href="#" className="snipcart-user-profile">
+      <span className="snipcart-user-email">You're currently logged out.  Click here to log in.</span>
+    </LoginLink>
+  </LoginDiv>
+  </>
 )
 
 export default Header
